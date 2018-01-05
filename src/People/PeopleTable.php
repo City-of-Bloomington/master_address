53cbc837df16c9d9abc0da9045f240cdec22fc59
inghamn <inghamn@bloomington.in.gov>
2018-01-05 11:08:55 -0500

    People are now searchable

--- src/People/PeopleTable.php
+++ src/People/PeopleTable.php
@@ -10,6 +10,9 @@ use Blossom\Classes\TableGateway;
 
 class PeopleTable extends TableGateway
 {
+    public static $defaultSort = ['p.lastname'];
+    private $columns = ['contact_type', 'firstname', 'lastname', 'email', 'phone', 'agency'];
+
     public function __construct() { parent::__construct('people', 'Application\People\Person'); }
 
 	/**
@@ -19,7 +22,7 @@ class PeopleTable extends TableGateway
 	 * @param int $currentPage
 	 * @return array|Paginator
 	 */
-	public function find(array $fields=null, array $order=['lastname'], int $itemsPerPage=null, int $currentPage=null)
+	public function find(array $fields=null, array $order=null, int $itemsPerPage=null, int $currentPage=null)
 	{
         $select = $this->queryFactory->newSelect();
         $select->cols(['p.*'])
@@ -28,19 +31,44 @@ class PeopleTable extends TableGateway
 			foreach ($fields as $key=>$value) {
 				switch ($key) {
 					case 'user_account':
-						if ($value) {
-							$select->where('username is not null');
-						}
-						else {
-							$select->where('username is null');
-						}
+                        $select->where($value ? 'username is not null' : 'username is null');
 					break;
 
 					default:
-                        $select->where("$key=?", $value);
+                        if (in_array($key, $this->columns)) {
+                            $select->where("$key=?", $value);
+                        }
 				}
 			}
 		}
+
+		if (!$order) { $order = self::$defaultSort; }
+        $select->orderBy($order);
+		return parent::performSelect($select, $itemsPerPage, $currentPage);
+	}
+
+	public function search(array $fields=null, array $order=null, int $itemsPerPage=null, int $currentPage=null)
+	{
+        $select = $this->queryFactory->newSelect();
+        $select->cols(['p.*'])
+               ->from('people as p');
+		if (count($fields)) {
+			foreach ($fields as $key=>$value) {
+				switch ($key) {
+					case 'user_account':
+                        $select->where($value ? 'username is not null' : 'username is null');
+					break;
+
+					default:
+                        if (in_array($key, $this->columns)) {
+                            $select->where("$key like ?", "$value%");
+                        }
+				}
+			}
+		}
+
+		if (!$order) { $order = self::$defaultSort; }
+        $select->orderBy($order);
 		return parent::performSelect($select, $itemsPerPage, $currentPage);
 	}
 }
