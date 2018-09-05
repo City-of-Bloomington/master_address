3efcdff6994f40fd91265baa089900ec53d323d9
inghamn <inghamn@bloomington.in.gov>
2018-09-05 16:37:26 -0400

    Streets: renamed "correct" action to "update"
    Updates #26

--- src/Application/Streets/Views/UpdateView.php
+++ src/Application/Streets/Views/UpdateView.php
@@ -11,27 +11,27 @@ use Application\Template;
 
 use Domain\Addresses\UseCases\Search\SearchResponse;
 use Domain\Streets\UseCases\Info\InfoResponse;
-use Domain\Streets\UseCases\Correct\CorrectRequest;
+use Domain\Streets\UseCases\Update\UpdateRequest;
 use Domain\Streets\Metadata;
 use Domain\People\Entities\Person;
 
-class CorrectView extends Template
+class UpdateView extends Template
 {
-    public function __construct(CorrectRequest $request,
+    public function __construct(UpdateRequest  $request,
                                 InfoResponse   $info,
                                 Metadata       $metadata,
                                 SearchResponse $addressSearch,
                                 ?Person        $contact=null)
     {
         parent::__construct('two-column', 'html');
-        $this->vars['title'] = $this->_('correct');
+        $this->vars['title'] = $this->_('update');
 
         $vars = ['towns' => $metadata->towns()];
         foreach ($request as $k=>$v) { $vars[$k] = parent::escape($v); }
         if ($contact && $contact->id === $request->contact_id) {
             $vars['contact_name'] = $contact->__toString();
         }
-        $this->blocks[] = new Block('streets/actions/correctForm.inc', $vars);
+        $this->blocks[] = new Block('streets/actions/updateForm.inc', $vars);
 
         $this->blocks[] = new Block('logs/changeLog.inc',            ['changes'      => $info->changeLog]);
         $this->blocks[] = new Block('streets/designations/list.inc', ['designations' => $info->designations]);
