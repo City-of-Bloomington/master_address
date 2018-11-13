4eb028592db692ca5fdf91235f444565bb05f925
inghamn <inghamn@bloomington.in.gov>
2018-11-13 16:19:53 -0500

    Implemented JS Chooser api

--- src/Application/People/Views/SearchView.php
+++ src/Application/People/Views/SearchView.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @copyright 2017-2018 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
 namespace Application\People\Views;
@@ -17,25 +17,26 @@ class SearchView extends Template
 {
     public function __construct(SearchResponse $response, int $itemsPerPage, int $currentPage)
     {
-        $format = !empty($_REQUEST['format']) ? $_REQUEST['format'] : 'html';
-        parent::__construct('default', $format);
+        $template = !empty($_REQUEST['callback']) ? 'callback'          : 'default';
+        $format   = !empty($_REQUEST['format'  ]) ? $_REQUEST['format'] : 'html';
+        parent::__construct($template, $format);
 
         $this->vars['title'] = $this->_('people_search');
         if ($response->errors) {
             $_SESSION['errorMessages'] = $response->errors;
         }
 
-        $vars = ['people' => $response->people];
+        $this->blocks[] = new Block('people/findForm.inc', [
+            'people'        => $response->people,
+            'callback_url'  => !empty($_GET['callback_url'  ]) ?        new Url($_GET['callback_url'  ]) : null,
+            'callback_field'=> !empty($_GET['callback_field']) ? parent::escape($_GET['callback_field']) : 'person_id',
+            'callback_js'   => !empty($_GET['callback'      ]) ? parent::escape($_GET['callback'      ]) : null,
 
-        $fields = ['firstname', 'lastname', 'email'];
-        foreach ($fields as $f) {
-            $vars[$f] = !empty($_GET[$f]) ? parent::escape($_GET[$f]) : '';
-        }
-        $vars['hidden'    ] = parent::filterActiveParams($_GET, $fields);
-        $vars['callback_url'  ] = !empty($_GET['callback_url'  ]) ?        new Url($_GET['callback_url'  ]) : null;
-        $vars['callback_field'] = !empty($_GET['callback_field']) ? parent::escape($_GET['callback_field']) : 'person_id';
-
-        $this->blocks[] = new Block('people/findForm.inc', $vars);
+            'firstname'     => !empty($_GET['firstname'     ]) ? parent::escape($_GET['firstname'     ]) : '',
+            'lastname'      => !empty($_GET['lastname'      ]) ? parent::escape($_GET['lastname'      ]) : '',
+            'email'         => !empty($_GET['email'         ]) ? parent::escape($_GET['email'         ]) : '',
+            'hidden'        => parent::filterActiveParams($_GET, ['firstname', 'lastname', 'email']),
+        ]);
 
         if ($response->total > $itemsPerPage) {
             $this->blocks[] = new Block('pageNavigation.inc', [
