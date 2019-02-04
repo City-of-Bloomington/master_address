58726c76a3cf6d1ca4d34e62e86f15ba57ebcc89
inghamn <inghamn@bloomington.in.gov>
2019-02-04 15:10:25 -0500

    Purged old, obsolete files

--- scripts/migration/r560-r561/udateAuthentication.sql
+++
@@ -1,2 +0,0 @@
-alter table master_address.users modify authenticationMethod varchar(40) default 'Employee';
-update master_address.users set authenticationMethod='Employee' where authenticationMethod='LDAP';
