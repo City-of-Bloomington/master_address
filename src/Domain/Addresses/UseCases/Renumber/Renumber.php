b9a0b6b612867982a2dd5e7e5d53941042ca21b6
inghamn <inghamn@bloomington.in.gov>
2019-07-29 11:08:58 -0400

    Street::renumber added duplicate address validation
    Updates #101

--- src/Domain/Addresses/UseCases/Renumber/Renumber.php
+++ src/Domain/Addresses/UseCases/Renumber/Renumber.php
@@ -2,7 +2,7 @@
 /**
  * Action to change the street numbers for many addresses at once
  *
- * @copyright 2018 City of Bloomington, Indiana
+ * @copyright 2018-2019 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/agpl-3.0.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -43,10 +43,31 @@ class Renumber
     private function validate(array $address_numbers): array
     {
         $errors = [];
+
+        $first     = current($address_numbers);
+        $a         = $this->repo->load($first->address_id);
+        $street_id = $a->street_id;
+
         foreach ($address_numbers as $a) {
             if (!$a->address_id   ) { $errors[] = 'addresses/missingId'; }
             if (!$a->street_number) { $errors[] = 'addresses/missingStreetNumber'; }
+
+            if ($this->isDuplicateAddress($street_id, $a)) { $errors[] = 'addresses/duplicateAddress'; }
         }
         return $errors;
+    }
+
+    private function isDuplicateAddress(int $street_id, AddressNumber $a): bool
+    {
+        $result = $this->repo->find([
+            'street_id'            => $street_id,
+            'street_number_prefix' => $a->street_number_prefix,
+            'street_number'        => $a->street_number,
+            'street_number_suffix' => $a->street_number_suffix
+        ]);
+        foreach ($result['rows'] as $r) {
+            if ($r->id != $a->address_id) { return true; }
+        }
+        return false;
     }
 }
