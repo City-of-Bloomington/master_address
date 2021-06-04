aaaf3f78766f259e65ee0d8d226476d2591c916b
Cliff Ingham <inghamn@bloomington.in.gov>
2021-06-04 15:23:43 -0400

    Fixed: location_status reference in InfoResponse
    The InfoResponses for Address and Subunits were still referring to location status.
    
    Fixes #149

--- src/Domain/Addresses/UseCases/Info/InfoResponse.php
+++ src/Domain/Addresses/UseCases/Info/InfoResponse.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2018-2019 City of Bloomington, Indiana
+ * @copyright 2018-2021 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -21,7 +21,7 @@ class InfoResponse
     public function activeCurrentLocation(): Location
     {
         foreach ($this->locations as $l) {
-            if ($l->active && $l->status == 'current') {
+            if ($l->active) {
                 return $l;
             }
         }
--- src/Domain/Subunits/UseCases/Info/InfoResponse.php
+++ src/Domain/Subunits/UseCases/Info/InfoResponse.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2018-2020 City of Bloomington, Indiana
+ * @copyright 2018-2021 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -20,7 +20,7 @@ class InfoResponse
     public function activeCurrentLocation(): Location
     {
         foreach ($this->locations as $l) {
-            if ($l->active && $l->status == 'current') {
+            if ($l->active) {
                 return $l;
             }
         }
